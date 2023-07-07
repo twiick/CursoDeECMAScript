f3497eef7481ae6904f95fd194c3548bcb590eed
twiickcom <twiick.com@gmail.com>
2023-07-07 13:18:57 -0500

    Se crea un archivo para mostrar los tipos de funciones que pueden ser creadas dento de JavaScript. Functions y Arrow functions

---
+++ 01-arrow-function.js
@@ -0,0 +1,10 @@
+// Function
+function square(num){
+    return num*num;
+}
+// Arrow function
+const square=(num)=>{
+    return num*num;
+};
+// Arrow function con return implicito
+const square=num=>num*num;
