1b064ee2e80fe6b7337c20b2991be378b3d82ae6
twiickcom <twiick.com@gmail.com>
2023-07-07 14:38:03 -0500

    Se hace uso del archivo 04-rest-spread.js para aplicar los conceptos de Spread operator y Rest

--- 04-rest-spread.js
+++ 04-rest-spread.js
@@ -11,3 +11,19 @@ let user={
 }
 let {age,country}=user;
 console.log(age,country)
+
+// Spread operator
+let person={
+    name:"Diego", age:32
+};
+let country2="Mexico";
+let data={id:1, ...person, country2};
+console.log(data);
+
+// Rest
+function sum(num, ...values){
+    console.log(values);
+    console.log(num+values[0]);
+    return(num+values[0])
+}
+sum(55,23,3,435,4,2,23);
