840c5227c46e0d00a41a2747d3af5f22f494b226
twiickcom <twiick.com@gmail.com>
2023-07-07 14:16:45 -0500

    Se crea un archivo para hacer uso de la desestructuración de objetos y arrays

---
+++ 04-rest-spread.js
@@ -0,0 +1,13 @@
+// Array destructuring
+let fruits=["Apple","Banana"];
+let [a,b]=fruits;
+console.log(a);
+
+// Object destructuring
+let user={
+    username: "Oscar",
+    age: 25,
+    country: "Italia"
+}
+let {age,country}=user;
+console.log(age,country)
