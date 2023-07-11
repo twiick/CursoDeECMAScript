480bd224dfa369f3a20269e7cf44f8d05320dcf5
twiickcom <twiick.com@gmail.com>
2023-07-11 12:46:24 -0500

    Se crea un archivo para poner práctica el concepto de promise-allsettled

---
+++ ES11/03-promise-allsettled.js
@@ -0,0 +1,9 @@
+const promise1=new Promise((resolve,reject)=>{
+    reject("Reject")
+})
+const promie2=new Promise((resolve,reject)=>{
+    resolve("Resolve")
+})
+const promise3=new Promise((resolve,reject)=>{
+    resolve("Resolve 2")
+})
